d8a5ea6ef8dbc41cca53d0859cfcb7344a557953
webliam <67931798+webliam@users.noreply.github.com>
2020-11-05 22:05:30 -0500

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/Main.js
@@ -0,0 +1,8 @@
+window.alert("Hello! his is my project!!, accept it to see my assignment");
+document.write("If you want to see my project , Please click in \"Click Me!\"");
+write.document="ddd";
+function My_First_Function() {
+    var str="This is string";
+    var result=str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML=result;
+}
