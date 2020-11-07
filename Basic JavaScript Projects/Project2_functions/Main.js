53f54d66eed93589c1b0d94da8863133fa6206db
webliam <67931798+webliam@users.noreply.github.com>
2020-11-07 14:57:17 -0500

    Add files via upload

---
+++ Basic JavaScript Projects/Project2_functions/Main.js
@@ -0,0 +1,7 @@
+
+function My_First_Function() { //defining  a function name
+    var x = 10; //assign the value 10 to x
+    x += 5;//add 5 to x
+
+    document.getElementById("Concatenate").innerHTML=x; //result with "Concatenate" id
+}
