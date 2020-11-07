9bbcabd2b552fa5446750b36e05959cd14e0f396
webliam <67931798+webliam@users.noreply.github.com>
2020-11-07 14:19:32 -0500

    Update Main.js

--- Basic JavaScript Projects/Project1_expressions_alert/Main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/Main.js
@@ -2,7 +2,10 @@ window.alert("Hello! This is my project!!, accept it to see my assignment"); //d
 document.write("If you want to see my project , Please click in \"Click Me!\""); // Write Notification
 write.document="ddd";
 function My_First_Function() { //defining  a function name
-    var str="This is my project!!"; //defin variable
+    var str1="This is my project!!"; //defin variable
+    var str2="Do you like it?"; //defin variable
+    var str=str1+str2; //defin variable
+
     var result=str.fontcolor("green"); // used fontcolor method
     document.getElementById("Green_Text").innerHTML=result; //result with "text_Gree" id
 }
