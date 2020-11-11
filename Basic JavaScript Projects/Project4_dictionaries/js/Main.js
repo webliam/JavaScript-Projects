5fcece5209507853e43a533838e44a30bcb508d4
webliam <67931798+webliam@users.noreply.github.com>
2020-11-10 23:10:10 -0500

    Update Main.js

--- Basic JavaScript Projects/Project4_dictionaries/js/Main.js
+++ Basic JavaScript Projects/Project4_dictionaries/js/Main.js
@@ -1,17 +1,17 @@
-function Vehicle(Make, Model, Year, Color) {
-  this.Vehicle_Make=Make;
-  this.Vehicle_Model=Model;
-  this.Vehicle_Year=Year;
-  this.Vehicle_Color=Color;
+function Vehicle(Make, Model, Year, Color) { //defined function
+  this.Vehicle_Make=Make; //defined Make
+  this.Vehicle_Model=Model; //defined Model
+  this.Vehicle_Year=Year; //defined year
+  this.Vehicle_Color=Color; //defined color
 }
-var Jack=new Vehicle("Dodge","Viper", 2020, "Red");
+var Jack=new Vehicle("Dodge","Viper", 2020, "Red"); 
 var Erik=new Vehicle("Jeep","Car", 2021, "Black");
 var Jan=new Vehicle("Dodge","Pinto", 2018, "White");
 function My_Function() { //defining  a function name 
 
-  document.getElementById("Concatenate").innerHTML="Erik Drive a "+ Erik.Vehicle_Color +" " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;//display the results of the constructor function within an HTML element
+  document.getElementById("Concatenate").innerHTML="Erik Drive a "+ Erik.Vehicle_Color +" " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;//display the results of the  function 
   delete Erik.Vehicle_Color;
-  document.getElementById("Concatenate1").innerHTML="After Delete: Erik Drive a "+ Erik.Vehicle_Color +" " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;//display the results of the constructor function within an HTML element
+  document.getElementById("Concatenate1").innerHTML="After Delete: Erik Drive a "+ Erik.Vehicle_Color +" " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
 
 }
 
