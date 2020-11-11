813ae7792b8221b80d3c5ebcf545680c3d2329ea
webliam <67931798+webliam@users.noreply.github.com>
2020-11-10 23:03:40 -0500

    Update Main.js

--- Basic JavaScript Projects/Project4_dictionaries/js/Main.js
+++ Basic JavaScript Projects/Project4_dictionaries/js/Main.js
@@ -1,14 +1,18 @@
-function my_dictionary() {
-    var animal = {
-      color="black",
-      age="4",
-      sound="bark!",
+function Vehicle(Make, Model, Year, Color) {
+  this.Vehicle_Make=Make;
+  this.Vehicle_Model=Model;
+  this.Vehicle_Year=Year;
+  this.Vehicle_Color=Color;
+}
+var Jack=new Vehicle("Dodge","Viper", 2020, "Red");
+var Erik=new Vehicle("Jeep","Car", 2021, "Black");
+var Jan=new Vehicle("Dodge","Pinto", 2018, "White");
+function My_Function() { //defining  a function name 
 
-    };
-    delete animal.sound;
-    document.getElementById("dictionary").innerHTML=animal.sound;
-    document.getElementById("dictionary").innerHTML =
-"color " + color + ". age " + age;
-  }
+  document.getElementById("Concatenate").innerHTML="Erik Drive a "+ Erik.Vehicle_Color +" " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;//display the results of the constructor function within an HTML element
+  delete Erik.Vehicle_Color;
+  document.getElementById("Concatenate1").innerHTML="After Delete: Erik Drive a "+ Erik.Vehicle_Color +" " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;//display the results of the constructor function within an HTML element
+
+}
 
   
