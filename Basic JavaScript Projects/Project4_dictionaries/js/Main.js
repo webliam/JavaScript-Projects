ba4e80e0d909a07b8693704193ff0d12cadb4d15
webliam <67931798+webliam@users.noreply.github.com>
2020-11-21 09:02:06 -0500

    Update Main.js

--- Basic JavaScript Projects/Project4_dictionaries/js/Main.js
+++ Basic JavaScript Projects/Project4_dictionaries/js/Main.js
@@ -1,11 +1,15 @@
 function my_dictionary() {
     var animal = {
-      color="black",
-      age="4",
-      sound="bark!",
+      color:"black",
+      age:"4",
+      sound:"bark!",
 
     };
+    document.getElementById("dictionary1").innerHTML="before delete sound ====> "+animal.sound;
+
     delete animal.sound;
-    document.getElementById("dictionary").innerHTML=animal.sound;
+    document.getElementById("dictionary").innerHTML="after delete sound ====> "animal.sound;
 
   }
+
+  
