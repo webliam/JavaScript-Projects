9d2aa87d625f5c5460f3e7054d08b2364e5c95da
webliam <67931798+webliam@users.noreply.github.com>
2020-11-20 21:23:34 -0500

    Update Main.js

--- Basic JavaScript Projects/Project4_dictionaries/js/Main.js
+++ Basic JavaScript Projects/Project4_dictionaries/js/Main.js
@@ -1,18 +1,11 @@
-function Vehicle(Make, Model, Year, Color) { //defined function
-  this.Vehicle_Make=Make; //defined Make
-  this.Vehicle_Model=Model; //defined Model
-  this.Vehicle_Year=Year; //defined year
-  this.Vehicle_Color=Color; //defined color
-}
-var Jack=new Vehicle("Dodge","Viper", 2020, "Red"); 
-var Erik=new Vehicle("Jeep","Car", 2021, "Black");
-var Jan=new Vehicle("Dodge","Pinto", 2018, "White");
-function My_Function() { //defining  a function name 
+function my_dictionary() {
+    var animal = {
+      color="black",
+      age="4",
+      sound="bark!",
 
-  document.getElementById("Concatenate").innerHTML="Erik Drive a "+ Erik.Vehicle_Color +" " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;//display the results of the  function 
-  delete Erik.Vehicle_Color;
-  document.getElementById("Concatenate1").innerHTML="After Delete: Erik Drive a "+ Erik.Vehicle_Color +" " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
+    };
+    delete animal.sound;
+    document.getElementById("dictionary").innerHTML=animal.sound;
 
-}
-
-  
+  }
